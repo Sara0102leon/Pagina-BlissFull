28fbf232c5ee61baa266a1294330acb2010eabd7
Sara0102leon <sara0102leon@gmail.com>
2026-09-15 15:22:02 -0400

    Notificaciones: volumen de sonido al 25% (suena fuerte al oido)

--- admin/core/app/layouts/layout.php
+++ admin/core/app/layouts/layout.php
@@ -341,7 +341,7 @@ $sys_open = $sys_active!="" ? " show" : "";
           if(!file){ return; }
           try{
             var a = new Audio(file);
-            a.volume = 0.2;
+a.volume = 0.25;
             a.play().catch(function(){});
           }catch(e){}
         }
